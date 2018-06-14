8193c22a516fde44f74f01c39f9b9c9b939c135d
sjhuang26 <sjhuang26@gmail.com>
2018-06-13 20:57:07 -0400

    Added CLI

--- powerschool.js
+++ powerschool.js
@@ -3,19 +3,25 @@ const promptly = require('promptly');
 const fileSystem = require('fs');
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
+const program = require('commander');
 
-let powerschool = async () => {
+let powerschool = async (username, password, shallow, screenshots, headless, outputDirectory) => {
   // INIT
+  if (username === undefined) {
+    username = await promptly.prompt('Username: ');
+  }
+  if (password === undefined) {
+    password = await promptly.password('Password: ');
+  }
 
-  // Nope, you aren't going to find my PowerSchool password here!
-  const password = await promptly.prompt("Password: ");
-
-  rimraf.sync('output');
-  mkdirp.sync('output');
+  rimraf.sync(outputDirectory);
+  mkdirp.sync(outputDirectory);
 
   // LOAD BROWSER
-  console.log("Loading browser...");
-  const browser = await puppeteer.launch();
+  console.log('Loading browser...');
+  const browser = await puppeteer.launch({
+    headless: headless
+  });
   const page = await browser.newPage();
   await page.setViewport({
     width: 1500,
@@ -23,35 +29,25 @@ let powerschool = async () => {
   });
 
   // LOAD WEBSITE
-  console.log("Loading website...");
+  console.log('Loading website...');
   await page.goto('https://powerschool3.niskyschools.org/public/');
 
   // LOG IN
-  console.log("Logging in...");
+  console.log('Logging in...');
   await page.type('#fieldAccount', 'hua08355');
   await page.type('#fieldPassword', password);
   await page.click('#btn-enter-sign-in');
   await page.waitForNavigation();
 
   // TAKE SCREENSHOT
-  console.log("Taking screenshot...");
-  await page.screenshot({
+  console.log('Taking screenshot...');
+  if (screenshots) await page.screenshot({
     path: 'output/screenshot-main-page.png',
     fullPage: 'true'
   });
 
-  // GET LINKS
-  console.log("Getting links...");
-  const links = await page.evaluate(() => {
-    let links = [];
-    [...document.querySelectorAll('#quickLookup > table.linkDescList.grid > tbody > tr > td:nth-child(19) > a')].forEach(x => {
-      links.push(x.href);
-    });
-    return links;
-  });
-
   // SCRAPE MAIN PAGE
-  console.log("Scraping main page...");
+  console.log('Scraping main page...');
   let result = await page.evaluate(() => {
     let safeParseInt = a => {
       const b = parseInt(a.trim());
@@ -120,86 +116,108 @@ let powerschool = async () => {
     };
   });
 
-  // SCRAPE COURSE PAGES
-  console.log('Scraping course pages (' + links.length + ')...');
-  for (let i = 0; i < links.length; ++i) {
-    console.log('    scraping page ' + (i + 1) + ' of ' + links.length + '...');
-    const link = links[i];
-    await page.goto(link);
-    await page.screenshot({
-      path: 'output/screenshot-course-' + (i + 1) + '.png',
-      fullPage: 'true'
+  if (!shallow) {
+    // GET LINKS
+    console.log('Getting links...');
+    const links = await page.evaluate(() => {
+      let links = [];
+      [...document.querySelectorAll('#quickLookup > table.linkDescList.grid > tbody > tr > td:nth-child(19) > a')].forEach(x => {
+        links.push(x.href);
+      });
+      return links;
     });
-    Object.assign(result.courses[i], await page.evaluate(() => {
-      let safeParseInt = a => {
-        const b = parseInt(a.trim());
-        return isNaN(b) ? '' : b;
-      };
 
-      let safeParseIntContent = e => {
-        if (e === null || e === undefined) return '';
-        return safeParseInt(e.textContent);
-      }
-      
-      let safeTextContent = e => {
-        if (e === null || e === undefined) return '';
-        return e.textContent.trim();
-      };
+    // SCRAPE COURSE PAGES
+    console.log('Scraping course pages (' + links.length + ')...');
+    for (let i = 0; i < links.length; ++i) {
+      console.log('    scraping page ' + (i + 1) + ' of ' + links.length + '...');
+      const link = links[i];
+      await page.goto(link);
+      if (screenshots) await page.screenshot({
+        path: 'output/screenshot-course-' + (i + 1) + '.png',
+        fullPage: 'true'
+      });
+      Object.assign(result.courses[i], await page.evaluate(() => {
+        let safeParseInt = a => {
+          const b = parseInt(a.trim());
+          return isNaN(b) ? '' : b;
+        };
 
-      let course = {};
-
-      // ASSIGNMENTS
-      course.assignments = [];
-      if (safeTextContent(document.querySelector('#content-main > div.box-round > table:nth-child(6) > tbody > tr:nth-child(2) > td')) !== 'No assignments found.') {
-        for (const e of document.querySelectorAll('#content-main > div.box-round > table:nth-child(6) > tbody > tr:nth-child(n+2)')) {
-          const scoreTokens = safeTextContent(e.querySelector(':nth-child(9)')).split('/');
-          let points, totalPoints;
-          if (scoreTokens.length !== 2) {
-            if (scoreTokens.length === 1) {
-              points = safeParseInt(scoreTokens[0]);
-              totalPoints = '';
-            } else {
-              points = '';
-              totalPoints = '';
-            }
-          } else {
-            points = safeParseInt(scoreTokens[0]);
-            totalPoints = safeParseInt(scoreTokens[1]);
-          }
-          let flags = [];
-          for (const f of e.querySelectorAll(':nth-child(n+4):nth-child(-n+8) > img')) {
-            flags.push(String(f.alt).trim());
-          }
-          course.assignments.push({
-            dueDate: safeTextContent(e.querySelector(':nth-child(1)')),
-            category: safeTextContent(e.querySelector(':nth-child(2)')),
-            assignment: safeTextContent(e.querySelector(':nth-child(3)')),
-            flags: flags,
-            points: points,
-            totalPoints: totalPoints,
-            percentageGrade: safeParseIntContent(e.querySelector(':nth-child(10)')),
-            letterGrade: safeTextContent(e.querySelector(':nth-child(11)')),
-          });
+        let safeParseIntContent = e => {
+          if (e === null || e === undefined) return '';
+          return safeParseInt(e.textContent);
         }
-      }
+        
+        let safeTextContent = e => {
+          if (e === null || e === undefined) return '';
+          return e.textContent.trim();
+        };
 
-      // OTHER
-      const $description = document.querySelector('#content-main > div.box-round > table.linkDescList > tbody > tr:nth-child(2)');
-      course.name = safeTextContent($description.querySelector(':nth-child(1)'));
-      course.teacher = safeTextContent($description.querySelector(':nth-child(2)'));
-      course.expression = safeTextContent($description.querySelector(':nth-child(3)'));
+        let course = {};
 
-      return course;
-    }));
+        // ASSIGNMENTS
+        course.assignments = [];
+        if (safeTextContent(document.querySelector('#content-main > div.box-round > table:nth-child(6) > tbody > tr:nth-child(2) > td')) !== 'No assignments found.') {
+          for (const e of document.querySelectorAll('#content-main > div.box-round > table:nth-child(6) > tbody > tr:nth-child(n+2)')) {
+            const scoreTokens = safeTextContent(e.querySelector(':nth-child(9)')).split('/');
+            let points, totalPoints;
+            if (scoreTokens.length !== 2) {
+              if (scoreTokens.length === 1) {
+                points = safeParseInt(scoreTokens[0]);
+                totalPoints = '';
+              } else {
+                points = '';
+                totalPoints = '';
+              }
+            } else {
+              points = safeParseInt(scoreTokens[0]);
+              totalPoints = safeParseInt(scoreTokens[1]);
+            }
+            let flags = [];
+            for (const f of e.querySelectorAll(':nth-child(n+4):nth-child(-n+8) > img')) {
+              flags.push(String(f.alt).trim());
+            }
+            course.assignments.push({
+              dueDate: safeTextContent(e.querySelector(':nth-child(1)')),
+              category: safeTextContent(e.querySelector(':nth-child(2)')),
+              assignment: safeTextContent(e.querySelector(':nth-child(3)')),
+              flags: flags,
+              points: points,
+              totalPoints: totalPoints,
+              percentageGrade: safeParseIntContent(e.querySelector(':nth-child(10)')),
+              letterGrade: safeTextContent(e.querySelector(':nth-child(11)')),
+            });
+          }
+        }
+
+        // OTHER
+        const $description = document.querySelector('#content-main > div.box-round > table.linkDescList > tbody > tr:nth-child(2)');
+        course.name = safeTextContent($description.querySelector(':nth-child(1)'));
+        course.teacher = safeTextContent($description.querySelector(':nth-child(2)'));
+        course.expression = safeTextContent($description.querySelector(':nth-child(3)'));
+
+        return course;
+      }));
+    }
   }
 
+  // WRAP UP
   browser.close();
-  return result;
-};
-
-powerschool().then(value => {
-  fileSystem.writeFile('output/export.json', JSON.stringify(value), err => {  
+  fileSystem.writeFile('output/export.json', JSON.stringify(result), err => {  
     if (err) throw err;
     console.log('Done!');
   });
-});
+};
+
+program
+  .command('fetch')
+  .description('fetch grades from PowerSchool')
+  .option('-u, --username <username>', 'username')
+  .option('-p, --password <password>', 'password')
+  .option('-s, --shallow', 'only fetch main page')
+  .option('-v, --screenshots', 'add screenshots')
+  .option('-h, --no-headless', 'disable headless mode')
+  .action(options => {
+    powerschool(options.username, options.password, options.shallow, options.screenshots, options.headless, 'output');
+  });
+program.parse(process.argv);
